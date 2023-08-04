b52f4b0d600ac4229d9c904e345aa9ba580584a3
Esther Mueni <emueni97@gmail.com>
2023-08-04 21:01:35 +0200

    header, hero and about section done

--- src/main.js
+++ src/main.js
@@ -1,2 +1,6 @@
-import '../styles/style.css';
-
+import '../styles/modern-normalize.css'
+import '../styles/style.css'
+import '../styles/components/header.css'
+import '../styles/components/hero.css'
+import '../styles/components/about.css'
+import  '../styles/utils.css'
