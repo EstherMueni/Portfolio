0ec72f46ff839d42b40df45e677e63299bc806ac
Esther Mueni <emueni97@gmail.com>
2023-08-08 14:17:05 +0200

    changed the dark and white mode

--- src/main.js
+++ src/main.js
@@ -1,19 +1,18 @@
-import '../styles/modern-normalize.css'
-import '../styles/style.css'
-import '../styles/components/header.css'
-import '../styles/components/hero.css'
-import '../styles/components/about.css'
-import '../styles/components/featured.css'
-import '../styles/components/work.css'
-import '../styles/components/contact.css'
-import '../styles/components/footer.css'
-import '../styles/components/mobile-nav.css'
-import  '../styles/utils.css'
-
+import '../styles/modern-normalize.css';
+import '../styles/style.css';
+import '../styles/components/header.css';
+import '../styles/components/hero.css';
+import '../styles/components/about.css';
+import '../styles/components/featured.css';
+import '../styles/components/work.css';
+import '../styles/components/contact.css';
+import '../styles/components/footer.css';
+import '../styles/components/mobile-nav.css';
+import '../styles/utils.css';
 
 import mobileNav from './utils/mobile-nav';
 import darkMode from './utils/dark-mode';
-import lazyLoading from './utils/lazy-loading'
+import lazyLoading from './utils/lazy-loading';
 
 mobileNav();
 darkMode();
--- src/utils/dark-mode.js
+++ src/utils/dark-mode.js
@@ -4,26 +4,24 @@ const darkMode = () => {
   // State
   const theme = localStorage.getItem('theme');
 
-  //On mount
+  // On mount
   theme && document.body.classList.add(theme);
 
   // Handlers
   const handleThemeToggle = () => {
     document.body.classList.toggle('light-mode');
     if (document.body.classList.contains('light-mode')) {
-      localStorage.setItem('theme','light-mode');
-        } else {
+      localStorage.setItem('theme', 'light-mode');
+    } else {
       localStorage.removeItem('theme');
       document.body.removeAttribute('class');
     }
   };
 
-
-  //Events
+  // Events
   themeToggleBtns.forEach(btn =>
-    btn.addEventListener('click', () => handleThemeToggle)
-    );
+    btn.addEventListener('click', handleThemeToggle)
+  );
 };
 
-
-export default darkMode();
+export default darkMode;
